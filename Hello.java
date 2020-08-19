e71ab4fd7d8a0242b082fd2094b9c12e013c26c3
kwonh9898 <69881537+kwonh9898@users.noreply.github.com>
2020-08-18 21:52:04 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+****************************************
+*Hello.java
+****************************************
+*Hanna Kwon
+*08182020
+*CMSC 255 - C90
+****************************************
+
+public class Hello {
+	public static void main(String[]args) {
+
+		System.out.println("<Hanna Kwon>");
+		System.out.println("<Hello.java>");
+		System.out.println("<Lab 1>");
+		System.out.println("<08182020>");
+		System.out.println("<CMSC 255 - C90>");
+		System.out.println();
+	}
+}
